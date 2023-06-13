fe843f9b9a808fda4512353d5d76fd95f19cedfd
강경규 <qwzx16@naver.com>
2023-06-13 19:06:16 +0900

    함수, 변수명 수정

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ function App() {
   let post = '강남 우동 asdads맛집';
   let mainLogo = 'Blog';
   let [글제목, 제목수정] = useState(['남자 여름 옷 추천', '배고플 땐 햄버거', '하지만 난 우동이 좋아..']);
-  let [날짜, 바뀔날짜] = useState(['3월 25일 발행', '3월 23일 발행', '3월 29일 발행']);
-  let [좋아요, up] = useState(0);
-
-
+  let [날짜, 바뀔날짜] = useState(['3월 25일', '3월 23일', '3월 29일']);
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(0);
 
   return (
     <div className="App">
@@ -19,46 +18,31 @@ function App() {
         <h4>{ mainLogo }</h4>
       </div>
       <div className='list-container'>
-        <div className='list'>
-          <h4>{ 글제목[0] }
-            <span className='btn' onClick={ ()=>{ up(좋아요+1)} }>👍</span>
-            <span className='btn' onClick={ ()=>{ up(좋아요-1)} }>👎</span>
-            <span>{ 좋아요 }</span>
-            <span className='btn' onClick={ ()=>{
-              let copyUpdate = [...글제목]
-              copyUpdate[0] = '여자 옷 추천'
-              제목수정(글제목 = copyUpdate);
-            } }>수정하기</span>
-          </h4>
-          <p>{ 날짜[0] }</p>
+        <div className='list btn'>
+          <h4 onClick={()=>{setModal(modal+1)}}>{글제목[0]}</h4>
+          <p>{날짜[0]}</p>
+          <span className='btn' onClick={()=>{setLike(like + 1)}}>UP</span>
+          <span className='btn' onClick={()=>{setLike(like - 1)}}>DOWN</span>
+          <span>{like}</span>
         </div>
-        <div className='list'>
-          <h4>{ 글제목[1] }</h4>
-          <p>{ 날짜[1] }</p>
-        </div>
-        <div className='list'>
-          <h4>{ 글제목[2] }</h4>
-          <p>{ 날짜[2] }</p>
-        </div>
-      </div>      
-      <Modal></Modal>
+      </div>
+      {
+        modal%2 == 1 ? <Modal></Modal> : null
+      } 
     </div>
   );
 }
-
 function Modal(){
   return(
-  <div className='flex-con'>
-  <div className='modal-con'>
-    <div>
-    <h3>글제목</h3>
-    <p>날짜</p>
-    <p>상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용</p>
+    <div className='flex-box'>
+      <div className='modal-con'>
+        <h3>제목</h3>
+        <p>날짜</p>
+        <p>상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내
+        용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용상세내용
+        상세내용상세내용상세내용상세내용상세내용상세내용</p>
+      </div>
     </div>
-  </div>
-  </div>
-
   )
 }
-
 export default App;
